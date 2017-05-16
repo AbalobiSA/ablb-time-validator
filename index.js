f9e7fab44ca6c154dd0ef141e2075ad9e8c1c71f
Carl Eiserman <carleiserman@gmail.com>
2017-05-16 13:24:46 +0200

    feature: added logging

--- index.js
+++ index.js
@@ -5,12 +5,27 @@ let salesforce = require("./components/mod_salesforce.js");
 let fs = require("fs");
 let odk = require("./components/mod_odk.js");
 let postgres = require("./components/mod_postgress.js");
+let winston = require("winston");
+
+/*============================================================================
+    Setup Winston
+ ============================================================================*/
+
+winston.add(winston.transports.File, {
+    filename: './logs/winston.log',
+    maxsize: 200000
+});
+
+winston.remove(winston.transports.Console);
 
 /*============================================================================
     Globals
  ============================================================================*/
 
-let TIMEOUT_MILIS = 3000;
+let TIMEOUT_MILIS;
+let PROCESS_START_TIME;
+let PROCESS_END_TIME;
+let QUERY_ITERATOR;
 
 /*============================================================================
     Run
@@ -23,37 +38,44 @@ main();
  ============================================================================*/
 
 function main(){
-    console.log("Abalobi Response Time Checker");
+
+    console.log((new Date().getTime()) + " Running - Abalobi Response Time Checker");
+
+    QUERY_ITERATOR = 0;
+    TIMEOUT_MILIS = 1000;
+
     postToAggregate();
+
+
 }
 
 /*============================================================================
     Test Functions
  ============================================================================*/
 
-function queryUsers(){
-    console.log("Creating Query...");
-    let query = "SELECT Id, FirstName, LastName, primary_community__c, FullPhotoUrl FROM User ";
-    salesforce.createQuery(query, function(response){
-        fs.writeFile("output.json", JSON.stringify(response, null, 4), function(){
-            console.log("FILE WRITTEN!");
-        })
-    }, function(error){
-        console.log(error);
-    });
-}
+// function queryUsers(){
+//     console.log("Creating Query...");
+//     let query = "SELECT Id, FirstName, LastName, primary_community__c, FullPhotoUrl FROM User ";
+//     salesforce.createQuery(query, function(response){
+//         fs.writeFile("output.json", JSON.stringify(response, null, 4), function(){
+//             console.log("FILE WRITTEN!");
+//         })
+//     }, function(error){
+//         console.log(error);
+//     });
+// }
 
-function query_ODK_ID(input){
-    console.log("Creating Query...");
-    let query = "SELECT Id, FirstName, LastName, primary_community__c, FullPhotoUrl FROM Ablb_Fisher_Trip__c ";
-    salesforce.createQuery(query, function(response){
-        fs.writeFile("output.json", JSON.stringify(response, null, 4), function(){
-            console.log("FILE WRITTEN!");
-        })
-    }, function(error){
-        console.log(error);
-    });
-}
+// function query_ODK_ID(input){
+//     console.log("Creating Query...");
+//     let query = "SELECT Id, FirstName, LastName, primary_community__c, FullPhotoUrl FROM Ablb_Fisher_Trip__c ";
+//     salesforce.createQuery(query, function(response){
+//         fs.writeFile("output.json", JSON.stringify(response, null, 4), function(){
+//             console.log("FILE WRITTEN!");
+//         })
+//     }, function(error){
+//         console.log(error);
+//     });
+// }
 
 /*============================================================================
     OpenFn - Submit Trip
@@ -65,11 +87,13 @@ function postToAggregate(){
         odk.post(function(response, odkID){
             if (response.statusCode === 200){
                 console.log("ODK Post Successful!");
+                startTimer();
                 queryPostGres(`'uuid:faketrip${odkID}'`);
             }
         });
     } catch (ex){
         console.log("Post unsuccessful!");
+        winston.log("debug" , elapsed_time("failure"));
     }
 
     // This will always fail, if you want to test failure
@@ -95,8 +119,11 @@ function queryPostGres(odkID, config){
 
     postgres.query(PGQUERY, null, function(err, res) {
         if(err) {
+            winston.log("debug" , elapsed_time("failure"));
             return console.error('error running query', err);
         }
+
+
 
         // console.log('number:', res);
         function queryAgain(){
@@ -107,31 +134,95 @@ function queryPostGres(odkID, config){
 
         if (res.rowCount === 0) {
             console.log(`No results received, checking again in ${ TIMEOUT_MILIS/1000 } seconds.`);
+            if (QUERY_ITERATOR < 20){
+                setTimeout(queryAgain , TIMEOUT_MILIS);
+            } else {
+                // Stop
+                winston.log("debug" , elapsed_time("failure"));
 
-            setTimeout(queryAgain , TIMEOUT_MILIS);
+            }
+
 
 
         } else {
+
+            winston.log("info" , elapsed_time("success"));
+
             fs.writeFile("heroku_response.json", JSON.stringify(res, null, 4), function(){
                 console.log("Heroku query successful! Check heroku_response.json");
                 console.log("Heroku query successful! Printing results:\n");
                 console.log(`Salesforce ID: ${res.rows[0].sfid}\n` + `Main Fisher ID: ${res.rows[0].main_fisher_id__c}`);
+                PROCESS_END_TIME = process.hrtime();
+                // calculateRunningTime(PROCESS_START_TIME, PROCESS_END_TIME);
+
                 postgres.close();
             });
         }
     });
 }
 
+/**
+ * Method used to stagger / back off on the amount of times we check
+ * for the record in the PG database.
+ */
 function recalculateTimeout(){
     // console.log(`Timeout Value: ${TIMEOUT_MILIS}`);
-    switch (true){
-        case (TIMEOUT_MILIS >= 3000 && TIMEOUT_MILIS < 18000): TIMEOUT_MILIS += 3000;
-        break;
-        case (TIMEOUT_MILIS >= 18000 && TIMEOUT_MILIS < 30000): TIMEOUT_MILIS += 5000;
-        break;
+    // switch (true){
+    //     case (TIMEOUT_MILIS >= 3000 && TIMEOUT_MILIS < 18000): TIMEOUT_MILIS += 3000;
+    //     break;
+    //     case (TIMEOUT_MILIS >= 18000 && TIMEOUT_MILIS < 30000): TIMEOUT_MILIS += 5000;
+    //     break;
+    // }
+
+    if (QUERY_ITERATOR < 12){
+        QUERY_ITERATOR++;
+        TIMEOUT_MILIS = 1000;
+    } else{
+        TIMEOUT_MILIS += 3000;
+        QUERY_ITERATOR++;
     }
-    if (TIMEOUT_MILIS >= 30000){
+    if (TIMEOUT_MILIS >= 30000) {
         TIMEOUT_MILIS = 30000;
     }
     // console.log(`Recalculated to: ${TIMEOUT_MILIS}`);
 }
+
+/*============================================================================
+    Process Timer Methods
+ ============================================================================*/
+
+/**
+ * Starts / Resets the countdown timer
+ */
+function startTimer(){
+    PROCESS_START_TIME = process.hrtime();
+}
+
+/**
+ * Returns an object showing seconds and miliseconds since timer reset
+ * @returns {{seconds: *, milis: string, fulltime: number}}
+ */
+function elapsed_time(status){
+    if (PROCESS_START_TIME === undefined){
+        PROCESS_START_TIME = process.hrtime();
+    }
+
+    let precision = 3; // 3 decimal places
+    let elapsed = process.hrtime(PROCESS_START_TIME)[1] / 1000000; // divide by a million to get nano to milli
+    // console.log(process.hrtime(PROCESS_START_TIME)[0] + " s, " + elapsed.toFixed(precision) + " ms - " + note); // print message + time
+
+    return {
+        "seconds" : process.hrtime(PROCESS_START_TIME)[0],
+        "milis" : elapsed.toFixed(precision),
+        "fulltime" : ((process.hrtime(PROCESS_START_TIME)[0]) * 1000) + parseFloat(elapsed.toFixed(precision)),
+        "status" : status
+    };
+}
+
+/*============================================================================
+    Logging functions
+ ============================================================================*/
+
+// function logOutput(output){
+//
+// }
