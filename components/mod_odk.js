d3abb8e3d87c85f1bf9850e8d698110778b3991a
Carl Eiserman <carleiserman@gmail.com>
2017-05-17 09:55:19 +0100

    feature: mapped secrets to server secrets file

--- components/mod_odk.js
+++ components/mod_odk.js
@@ -3,7 +3,7 @@
  ============================================================================*/
 
 let request = require("request");
-let secrets = require("../secrets/secrets");
+let secrets = require("../../../../secrets/secrets");
 let dataTemplate = require("./catchData/template");
 
 const OPENFN_URL = secrets.OPENFN_URL;
--- components/mod_salesforce.js
+++ components/mod_salesforce.js
@@ -2,7 +2,7 @@
  * Created by Carl on 2017-05-08.
  */
 const jsforce = require("jsforce");
-const secrets = require("../secrets/secrets.js");
+let secrets = require("../../../../secrets/secrets");
 
 function createQuery(queryString, success, error){
     const conn = new jsforce.Connection();
